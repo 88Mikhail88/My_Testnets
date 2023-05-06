743226e2a43fed4ce43224fc026022fe6e61241c
Mikhail Kupriyanov <101403541+88Mikhail88@users.noreply.github.com>
2023-05-06 12:01:38 +0300

    Add files via upload

---
+++ Shardeum/shardeum-sphinx 1.2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#rebuild + reset network MM
+
+#установить
+
+sudo apt-get install curl && sudo apt update -y && sudo apt install docker.io && docker --version
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && docker-compose --version
+
+curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
