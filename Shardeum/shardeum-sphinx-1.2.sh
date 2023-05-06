c3d5bc9dc23796f9c34afdf057c74bc43bd47f59
Mikhail Kupriyanov <101403541+88Mikhail88@users.noreply.github.com>
2023-05-06 12:12:15 +0300

    Update shardeum-sphinx-1.2.sh

--- Shardeum/shardeum-sphinx-1.2.sh
+++ Shardeum/shardeum-sphinx-1.2.sh
@@ -4,8 +4,13 @@
 
 #install
 
-sudo apt-get install curl && sudo apt update -y && sudo apt install docker.io && docker --version
+sudo apt-get install curl && sudo apt update -y
+sudo apt install docker.io
+sleep 2s
+docker --version
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && docker-compose --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
